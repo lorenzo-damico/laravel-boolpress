19decf7b35c7ca499bb1bb89cd7c92da73b28f72
lorenzo-damico <loredam@hotmail.it>
2020-11-09 17:34:02 +0100

    Aggiunto seeder articoli

--- database/seeds/ArticleSeeder.php
+++ database/seeds/ArticleSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Article;
+use App\User;
+use Illuminate\Support\Str;
 
 class ArticleSeeder extends Seeder
 {
@@ -9,8 +13,20 @@ class ArticleSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20; $i++) {
+
+          $user = User::inRandomOrder()->first();
+
+          $newArticle = new Article();
+
+          $newArticle->user_id = $user->id;
+          $newArticle->title = $faker->sentence(6, true);
+          $newArticle->content = $faker->paragraph(6, true);
+          $newArticle->slug = Str::of($newArticle->title)->slug('-');
+
+          $newArticle->save();
+        }
     }
 }
